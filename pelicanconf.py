12afd25d396587b9b1482b350375f63ae7563673
Ryan Mills <ryancmills2@gmail.com>
2018-01-04 00:13:41 -0500

    with theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,6 +9,7 @@ SITENAME = "Ryan's Blog"
 SITEURL = ''
 
 PATH = 'content'
+THEME = 'themes/minimalX'
 
 TIMEZONE = 'America/New_York'
 
@@ -32,7 +33,7 @@ MARKDOWN = {
     'output_format': 'html5',
 }
 
-STATIC_PATHS = ['images']
+STATIC_PATHS = ['images', 'authors']
 
 IGNORE_FILES = ['.ipynb_checkpoints']
 
@@ -43,10 +44,34 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/bosbraves'),
+          ('linkedin', 'https://www.linkedin.com/in/ryancmills'),)
+SHARE_BUTTONS = ('github', 'linkedin', 'twitter', 'mail')
 
-DEFAULT_PAGINATION = False
+# Pagination after more than 4 articles
+DEFAULT_PAGINATION = 4
+
+SUMMARY_MAX_LENGTH = 70
+
+USE_FOLDER_AS_CATEGORY = False
+DEFAULT_CATEGORY = 'General'
+
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# Plugins
+TAG_CLOUD = True
+TAG_CLOUD_STEPS = 5
+TAG_CLOUD_MAX_ITEMS = 50
+TAG_CLOUD_BADGE = True
+TAG_CLOUD_SORTING = 'size'
+
+# Plugins
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["tag_cloud"]
+
+# Author Pages
+AUTHOR_PAGE_PATH = 'authors'
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
